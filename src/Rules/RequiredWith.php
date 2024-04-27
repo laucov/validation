7e9274ee1e663d843d666dc42a73569d5b0505f0
covaleski <covaleski23@gmail.com>
2024-04-27 10:42:41 -0300

    feat: create `RequiredWith`

---
+++ src/Rules/RequiredWith.php
@@ -0,0 +1,68 @@
+<?php
+
+/**
+ * This file is part of Laucov's Validation Library project.
+ * 
+ * Copyright 2024 Laucov Serviços de Tecnologia da Informação Ltda.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * @package validation
+ * 
+ * @author Rafael Covaleski Pereira <rafael.covaleski@laucov.com>
+ * 
+ * @license <http://www.apache.org/licenses/LICENSE-2.0> Apache License 2.0
+ * 
+ * @copyright © 2024 Laucov Serviços de Tecnologia da Informação Ltda.
+ */
+
+namespace Laucov\Validation\Rules;
+
+use Laucov\Validation\AbstractRule;
+
+/**
+ * Requires a value to contain a non-empty value.
+ */
+#[\Attribute(\Attribute::TARGET_PROPERTY)]
+class RequiredWith extends AbstractRule
+{
+    /**
+     * Combination of keys that will require the value to be present.
+     * 
+     * @var array<int|string>
+     */
+    protected array $keys;
+
+    /**
+     * Create the rule instance.
+     */
+    public function __construct(int|string ...$keys)
+    {
+        $this->keys = $keys;
+    }
+
+    /**
+     * Validate a single value.
+     */
+    public function validate(mixed $value): bool
+    {
+        // Check if all required keys are present.
+        foreach ($this->keys as $key) {
+            if (!isset($this->getData()->$key)) {
+                return true;
+            }
+        }
+
+        return !empty($value);
+    }
+}
---
+++ tests/unit/Rules/RequiredWithTest.php
@@ -0,0 +1,97 @@
+<?php
+
+/**
+ * This file is part of Laucov's Validation Library project.
+ * 
+ * Copyright 2024 Laucov Serviços de Tecnologia da Informação Ltda.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * @package validation
+ * 
+ * @author Rafael Covaleski Pereira <rafael.covaleski@laucov.com>
+ * 
+ * @license <http://www.apache.org/licenses/LICENSE-2.0> Apache License 2.0
+ * 
+ * @copyright © 2024 Laucov Serviços de Tecnologia da Informação Ltda.
+ */
+
+declare(strict_types=1);
+
+namespace Tests\Unit\Rules;
+
+use Laucov\Validation\Rules\RequiredWith;
+use Tests\RuleTestCase;
+
+/**
+ * @coversDefaultClass \Laucov\Validation\Rules\RequiredWith
+ */
+class RequiredWithTest extends RuleTestCase
+{
+    public function dataProvider(): array
+    {
+        $data = [
+            'name' => 'John',
+            'age' => 42,
+            'email' => 'john.doe@foomail.net',
+        ];
+
+        return [
+            [['name'], $data, [0, 1, 2, 6, 8]],
+            [['name', 'age'], $data, [0, 1, 2, 6, 8]],
+            [['address'], $data, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]],
+            [['email', 'gender'], $data, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]],
+        ];
+    }
+
+    /**
+     * @covers ::__construct
+     * @covers ::getData
+     * @covers ::setData
+     * @covers ::validate
+     * @dataProvider dataProvider
+     */
+    public function testCanValidate(
+        array $constructor_args,
+        array $data,
+        array $expected,
+    ): void {
+        $rule = new RequiredWith(...$constructor_args);
+        $rule->setData($data);
+        $this->assertValidation($rule, $expected);
+    }
+
+    /**
+     * @coversNothing
+     */
+    public function testIsPropertyAttribute(): void
+    {
+        $this->assertIsPropertyAttribute(RequiredWith::class, false);
+    }
+
+    protected function getValues(): array
+    {
+        return [
+            0 => 'Some value',
+            1 => 478541245,
+            2 => ['foo', 'bar' => 'baz'],
+            3 => null,
+            4 => [],
+            5 => '',
+            6 => new \stdClass(),
+            7 => 0,
+            8 => true,
+            9 => false,
+        ];
+    }
+}
