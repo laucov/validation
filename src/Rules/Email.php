cdb72e25e704ed434941e595c3117c6f9698b474
covaleski <covaleski23@gmail.com>
2024-05-02 09:07:24 -0300

    feat: create `Email` rule

---
+++ src/Rules/Email.php
@@ -0,0 +1,63 @@
+<?php
+
+/**
+ * This file is part of Laucov's Validation Library project.
+ * 
+ * Copyright 2024 Laucov Serviços de Tecnologia da Informação Ltda.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * @package validation
+ * 
+ * @author Rafael Covaleski Pereira <rafael.covaleski@laucov.com>
+ * 
+ * @license <http://www.apache.org/licenses/LICENSE-2.0> Apache License 2.0
+ * 
+ * @copyright © 2024 Laucov Serviços de Tecnologia da Informação Ltda.
+ */
+
+namespace Laucov\Validation\Rules;
+
+use Laucov\Validation\AbstractRule;
+
+/**
+ * Requires a value to be a valid email address.
+ */
+#[\Attribute(\Attribute::TARGET_PROPERTY | \Attribute::IS_REPEATABLE)]
+class Email extends AbstractRule
+{
+    /**
+     * Create the rule instance.
+     */
+    public function __construct(
+        /**
+         * Whether to accept non-ASCII characters in the address.
+         */
+        protected bool $unicode = false,
+    ) {
+    }
+
+    /**
+     * Validate a single value.
+     */
+    public function validate(mixed $value): bool
+    {
+        if (!is_scalar($value)) {
+            return false;
+        }
+
+        $options = $this->unicode ? FILTER_FLAG_EMAIL_UNICODE : 0;
+
+        return filter_var($value, FILTER_VALIDATE_EMAIL, $options) !== false;
+    }
+}
---
+++ tests/unit/Rules/EmailTest.php
@@ -0,0 +1,87 @@
+<?php
+
+/**
+ * This file is part of Laucov's Validation Library project.
+ * 
+ * Copyright 2024 Laucov Serviços de Tecnologia da Informação Ltda.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * @package validation
+ * 
+ * @author Rafael Covaleski Pereira <rafael.covaleski@laucov.com>
+ * 
+ * @license <http://www.apache.org/licenses/LICENSE-2.0> Apache License 2.0
+ * 
+ * @copyright © 2024 Laucov Serviços de Tecnologia da Informação Ltda.
+ */
+
+declare(strict_types=1);
+
+namespace Tests\Unit\Rules;
+
+use Laucov\Validation\Rules\Email;
+use Tests\RuleTestCase;
+
+/**
+ * @coversDefaultClass \Laucov\Validation\Rules\Email
+ */
+class MyRuleTest extends RuleTestCase
+{
+    public function dataProvider(): array
+    {
+        return [
+            [[], [0, 1]],
+            [[false], [0, 1]],
+            [[true], [0, 1, 2, 3]],
+        ];
+    }
+
+    /**
+     * @covers ::__construct
+     * @covers ::validate
+     * @dataProvider dataProvider
+     */
+    public function testCanValidate(array $arguments, array $expected): void
+    {
+		$rule = new Email(...$arguments);
+        $this->assertValidation($rule, $expected);
+    }
+
+    /**
+     * @covers ::validate
+     * @uses Laucov\Validation\Rules\Email::__construct
+     */
+    public function testDoesNotAcceptNonScalarValues(): void
+    {
+        $this->assertRejectsNonScalarValues(new Email());
+    }
+
+    /**
+     * @coversNothing
+     */
+    public function testIsPropertyAttribute(): void
+    {
+        $this->assertIsPropertyAttribute(Email::class, true);
+    }
+
+    protected function getValues(): array
+    {
+        return [
+            0 => 'john.doe@foobar.com',
+            1 => 'MR-joseph-someone@somewhere.org',
+            2 => 'SR-josé-alguém@algum-lugar.org',
+            3 => '字@domain.com',
+        ];
+    }
+}
