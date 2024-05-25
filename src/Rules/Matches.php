10116d99710cf6176b28d95a20b1631c7a583edf
covaleski <covaleski23@gmail.com>
2024-05-25 15:38:00 -0300

    feat: create `Match` and `DoesNotMatch`

---
+++ src/Rules/Matches.php
@@ -0,0 +1,67 @@
+<?php
+
+/**
+ * This file is part of Laucov's Validation Library project.
+ * 
+ * Copyright 2024 Laucov Serviços de Tecnologia da Informação Ltda.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * @package validation
+ * 
+ * @author Rafael Covaleski Pereira <rafael.covaleski@laucov.com>
+ * 
+ * @license <http://www.apache.org/licenses/LICENSE-2.0> Apache License 2.0
+ * 
+ * @copyright © 2024 Laucov Serviços de Tecnologia da Informação Ltda.
+ */
+
+namespace Laucov\Validation\Rules;
+
+use Laucov\Validation\AbstractRule;
+
+/**
+ * Requires a value to match another in the context data.
+ */
+#[\Attribute(\Attribute::TARGET_PROPERTY | \Attribute::IS_REPEATABLE)]
+class Matches extends AbstractRule
+{
+    /**
+     * Create the rule instance.
+     */
+    public function __construct(
+        /**
+         * Key from the context data that the value will be compared to.
+         */
+        protected string $key,
+    ) {
+    }
+
+    /**
+     * Get the rule's info.
+     * 
+     * @return array<string>
+     */
+    public function getInfo(): array
+    {
+        return ['key' => $this->key];
+    }
+
+    /**
+     * Validate a single value.
+     */
+    public function validate(mixed $value): bool
+    {
+        return $value === ($this->data->{$this->key} ?? null);
+    }
+}
---
+++ tests/unit/Rules/DoesNotMatchTest.php
@@ -0,0 +1,117 @@
+<?php
+
+/**
+ * This file is part of Laucov's Validation Library project.
+ * 
+ * Copyright 2024 Laucov Serviços de Tecnologia da Informação Ltda.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * @package validation
+ * 
+ * @author Rafael Covaleski Pereira <rafael.covaleski@laucov.com>
+ * 
+ * @license <http://www.apache.org/licenses/LICENSE-2.0> Apache License 2.0
+ * 
+ * @copyright © 2024 Laucov Serviços de Tecnologia da Informação Ltda.
+ */
+
+declare(strict_types=1);
+
+namespace Tests\Unit\Rules;
+
+use Laucov\Validation\Rules\DoesNotMatch;
+use Tests\RuleTestCase;
+
+/**
+ * @coversDefaultClass \Laucov\Validation\Rules\DoesNotMatch
+ */
+class DoesNotMatchTest extends RuleTestCase
+{
+    /**
+     * Provides arguments and expectations for validation tests.
+     */
+    public function dataProvider(): array
+    {
+        $data = [
+            'a' => 'foo',
+            'b' => 123,
+            'c' => false,
+        ];
+
+        return [
+            [
+                ['a'],
+                $data,
+                ['key' => 'a'],
+                [1, 2, 3, 4, 5, 6, 7],
+            ],
+            [
+                ['b'],
+                $data,
+                ['key' => 'b'],
+                [0, 1, 2, 4, 5, 6, 7],
+            ],
+            [
+                ['c'],
+                $data,
+                ['key' => 'c'],
+                [0, 1, 2, 3, 4, 5, 7],
+            ],
+        ];
+    }
+
+    /**
+     * @covers ::__construct
+     * @covers ::getInfo
+     * @covers ::validate
+     * @uses Laucov\Validation\AbstractRule::setData
+     * @dataProvider dataProvider
+     */
+    public function testCanValidate(
+        array $arguments,
+        array $data,
+        array $expected_info,
+        array $expected_success,
+    ): void {
+        $rule = new DoesNotMatch(...$arguments);
+        $rule->setData($data);
+        $this->assertRuleInfo($rule, $expected_info);
+        $this->assertValidation($rule, $expected_success);
+    }
+
+    /**
+     * @coversNothing
+     */
+    public function testIsPropertyAttribute(): void
+    {
+        $this->assertIsPropertyAttribute(DoesNotMatch::class, true);
+    }
+
+    /**
+     * Get values to validate.
+     */
+    protected function getValues(): array
+    {
+        return [
+            0 => 'foo',
+            1 => 'bar',
+            2 => '123',
+            3 => 123,
+            4 => '0',
+            5 => 0,
+            6 => false,
+            7 => '',
+        ];
+    }
+}
